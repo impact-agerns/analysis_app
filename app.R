2a57e8efb9a0d81cd7f2c215f42c84a2f4253b13
Alvaro Gerns <49208751+agerns@users.noreply.github.com>
2024-10-31 18:06:07 +0100

    working analysis and table script.
    still need to include aggregation script and visualization.

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
-# Load required libraries
+# Required Libraries
 library(shiny)
+library(shinydashboard)
 library(dplyr)
 library(readxl)
 library(openxlsx)
@@ -7,41 +8,57 @@ library(janitor)
 library(tidyverse)
 library(purrr)
 
-ui <- fluidPage(
-	titlePanel("Data Processing and Analysis App"),
+# UI
+ui <- dashboardPage(
+	dashboardHeader(title = "Data Processing and Analysis"),
 	
-	sidebarLayout(
-		sidebarPanel(
-			fileInput("data_file", "Upload Data File (xlsx)", accept = ".xlsx"),
-			fileInput("kobo_file", "Upload Kobo Tool File (xlsx)", accept = ".xlsx"),
-			selectInput("disaggregate_by", "Choose Disaggregation Level", choices = NULL, multiple = TRUE),
-			actionButton("run_analysis", "Run Analysis"),
-			verbatimTextOutput("analysis_status"),  # For displaying analysis completion message
-			uiOutput("progress_bar"),  # For progress bar			
-			selectInput("view_type", "Choose View Type", choices = c("Raw Data" = "raw", "Analysis Data" = "analysis")),
+	dashboardSidebar(
+		sidebarMenu(
+			menuItem("Home", tabName = "home", icon = icon("home")),
+			menuItem("Table", tabName = "table", icon = icon("table")),
+			# selectInput("disaggregate_by", "Choose Disaggregation Level", choices = NULL, multiple = TRUE),
+			menuItem("Plot", tabName = "plot", icon = icon("chart-bar"))
+		)
+	),
+	
+	dashboardBody(
+		tabItems(
+			# Home Tab with File Inputs and Analysis Trigger
+			tabItem(tabName = "home",
+							fluidRow(
+								box(fileInput("data_file", "Upload Data File (xlsx)", accept = ".xlsx")),
+								box(fileInput("kobo_file", "Upload Kobo Tool File (xlsx)", accept = ".xlsx"))
+							),
+							verbatimTextOutput("analysis_status"),  # For displaying analysis completion message
+							uiOutput("progress_bar"),
+							box(selectInput("disaggregate_by", "Choose Disaggregation Level", choices = NULL, multiple = TRUE),
+									actionButton("run_analysis", "Run Analysis"),
+									verbatimTextOutput("analysis_status"),
+									uiOutput("progress_bar"))),
 			
-			# Analysis Filters
-			selectInput("x_axis", "X-Axis", choices = NULL),
-			selectInput("y_axis", "Y-Axis", choices = NULL),
-			selectInput("measure", "Measure", choices = c("mean", "count", "median")),
-			selectInput("plot_type", "Plot Type", choices = c("Bar Chart" = "bar", "Scatterplot" = "scatter", "Violin Plot" = "violin")),
+			# Table Tab
+			tabItem(tabName = "table",
+							fluidRow(
+								selectInput("view_type", "Data Type", choices = c("Raw Data" = "raw", "Analysis Data" = "analysis")),
+								downloadButton("download_data", "Download Processed Data"),
+								dataTableOutput("table_output"))),
 			
-			downloadButton("download_data", "Download Processed Data")
-		),
-		
-		mainPanel(
-			tabsetPanel(
-				tabPanel("Table", dataTableOutput("table_output")),
-				tabPanel("Analysis", plotOutput("plot_output"))
+			
+			# Plot Tab with Controls
+			tabItem(tabName = "plot",
+							fluidRow(
+								selectInput("plot_type", "Plot Type", choices = c("Bar Chart" = "bar", "Scatterplot" = "scatter", "Violin Plot" = "violin")),
+								selectInput("x_axis", "X-Axis", choices = NULL),
+								selectInput("y_axis", "Y-Axis", choices = NULL),
+								selectInput("measure", "Measure", choices = c("mean", "count", "median")),
+								plotOutput("plot_output"))
 			)
 		)
 	)
 )
 
-
-# Define the Server
 server <- function(input, output, session) {
-
+	
 	
 	# Reactive variable to store the loaded data
 	data_in <- reactiveVal(NULL)
@@ -79,19 +96,6 @@ server <- function(input, output, session) {
 		selectInput("disaggregate_by", "Choose Disaggregation Level", choices = choices, multiple = TRUE)
 	})
 	
-	# After loading the data
-	# observeEvent(input$data_file, {
-	# 	req(input$data_file)
-	# 	data <- readxl::read_excel(input$data_file$datapath)
-	# 	
-	# 	# Store the data in a reactive variable
-	# 	data_in(data)
-	# 	
-	# 	# Update choices for disaggregation level based on the data columns
-	# 	updateSelectInput(session, "disaggregate_by", 
-	# 										choices = names(data),
-	# 										selected = NULL)  # or specify any default selected options
-	# })
 	
 	# This code snippet integrates progress updates during the processing of data.
 	observeEvent(input$run_analysis, {
@@ -119,7 +123,8 @@ server <- function(input, output, session) {
 			}
 			
 			# Define disaggregation levels
-			dis <- list("admin2", c("admin2", "admin3"))
+			# dis <- list("admin2", c("admin2", "admin3"))
+			dis <- input$disaggregate_by
 			
 			# Clean expanded data
 			clean_expanded <- data_in() %>%
@@ -176,8 +181,16 @@ server <- function(input, output, session) {
 			df_res <- res %>% bind_rows() %>%
 				select(any_of(c("disag_var_1", "disag_val_1", "disag_var_2", "disag_val_2")), everything())
 			
+			df_res_labelled <- df_res %>% 
+				left_join(select(tool.combined, name, label, name.choice, label.choice, q.type), by=c("question"="name", "choice"="name.choice"))%>% 
+				# rename(aggregation_level1 = disag_var_1, aggregation_value1 = disag_val_1, 
+				# 			 aggregation_level2 = disag_var_2, aggregation_value2 = disag_val_2, 
+				# ) %>% 
+				# mutate(aggregation_level1 = ifelse(is.na(aggregation_level1), "all_settlements", aggregation_level1)) %>%
+				unique()
+			
 			# Output the results in the main panel
-			# output$table_output <- renderDataTable({ df_res })
+			df_res_labelled_reactive <<- reactive({ df_res_labelled })
 			
 			# Success message
 			output$analysis_status <- renderText("Analysis completed successfully!")
@@ -196,69 +209,20 @@ server <- function(input, output, session) {
 	
 	
 	
-	# df_res <- eventReactive(input$run_analysis, {  # Use eventReactive for the analysis
-	# 	req(data_in(), kobo_tool(), input$disaggregate_by)
-	# 	
-	# 	# Ensure disaggregation level is selected
-	# 	if (length(input$disaggregate_by) == 0) return(NULL)
-	# 	
-	# 	clean_expanded <- data_in()
-	# 	tool.combined <- kobo_tool()
-	# 	res <- list()
-	# 	dis <- input$disaggregate_by
-	# 	
-	# 	# Efficient aggregation
-	# 	for (d in dis) {
-	# 		d <- d %>% unlist
-	# 		df <- clean_expanded
-	# 		if (sum(d %in% names(df)) > 0) df <- df %>% group_by(across(any_of(d)))
-	# 		
-	# 		columns_to_pivot <- names(df) %>% str_detect("^col\\.sm|^col\\.so|col\\.int")
-	# 		if (sum(columns_to_pivot) == 0) next
-	# 		
-	# 		df <- df %>% 
-	# 			mutate(across(matches(paste0("^", c("col.sm", "col.so"), "\\.")), as.numeric), across(any_of("col.int"), as.numeric)) %>%
-	# 			summarise(across(matches(paste0("^", c("col.sm", "col.so"), "\\.")), list(mean=~mean(., na.rm=T), count=~sum(., na.rm=T), resp=~sum(!is.na(.)), n=~n()), .names="{.col}--{.fn}"),
-	# 								across(any_of("col.int"), list(mean=~mean(., na.rm=T), sum=~sum(., na.rm=T), median=~median(., na.rm=T), resp=~sum(!is.na(.)), n=~n()), .names="{.col}--{.fn}")) %>% 
-	# 			ungroup %>%
-	# 			pivot_longer(cols = names(df)[columns_to_pivot], names_to = "col", values_to = "value") %>%
-	# 			separate(name, c("col", "function"), "--", remove=FALSE) %>% 
-	# 			separate(col, c("question", "choice"), "\\.", remove=FALSE) %>% 
-	# 			select(-name) %>%
-	# 			pivot_wider(names_from="function", values_from="value")
-	# 		
-	# 		res[[paste(d, collapse = "_")]] <- df
-	# 	}
-	# 	
-	# 	# Combine and label data efficiently
-	# 	df_res <- res %>% bind_rows() %>% 
-	# 		select(any_of(c("disag_var_1", "disag_val_1", "disag_var_2", "disag_val_2")), everything())
-	# 	
-	# 	df_res_labelled <- df_res %>% 
-	# 		left_join(select(tool.combined, name, label, name.choice, label.choice, q.type), by=c("question"="name", "choice"="name.choice")) %>%
-	# 		rename(aggregation_level1 = disag_var_1, aggregation_value1 = disag_val_1, 
-	# 					 aggregation_level2 = disag_var_2, aggregation_value2 = disag_val_2) %>%
-	# 		mutate(aggregation_level1 = ifelse(is.na(aggregation_level1), "all_settlements", aggregation_level1)) %>%
-	# 		unique()
-	# 	
-	# 	df_res_labelled
-	# })
-	
-	
 	# Display raw or processed data in table based on view type
 	output$table_output <- renderDataTable({
 		req(input$view_type)
 		if (input$view_type == "raw") {
 			data_in()
 		} else {
-			df_res
+			df_res_labelled_reactive()
 		}
 	})
 	
 	# Display summary statistics for selected variables
 	output$stats_output <- renderTable({
-		req(df_res)
-		data_stats <- df_res %>% 
+		req(df_res_labelled_reactive())
+		data_stats <- df_res_labelled_reactive() %>% 
 			summarise(across(where(is.numeric), list(mean = ~ mean(.x, na.rm = TRUE), 
 																							 sd = ~ sd(.x, na.rm = TRUE),
 																							 min = ~ min(.x, na.rm = TRUE),
@@ -268,34 +232,53 @@ server <- function(input, output, session) {
 	
 	# Plot output based on user choices
 	output$plot_output <- renderPlot({
-		req(df_res, input$x_axis, input$y_axis)
+		req(input$x_axis, input$y_axis, input$plot_type)  # Ensure all inputs are selected
 		
-		plot_data <- df_res
+		# Dynamically select columns from data based on user input
+		x_var <- sym(input$x_axis)
+		y_var <- sym(input$y_axis)
 		
-		x_col <- sym(input$x_axis)
-		y_col <- sym(input$y_axis)
+		# Base ggplot object
+		p <- ggplot(data = data.in, aes(x = !!x_var, y = !!y_var))
 		
-		ggplot(plot_data, aes(x = !!x_col, y = !!y_col)) +
-			{
-				if (input$plot_type == "bar") geom_bar(stat = "identity")
-				else if (input$plot_type == "scatter") geom_point()
-				else geom_violin()
-			} +
-			labs(x = input$x_axis, y = input$y_axis)
+		# Conditional layer based on selected plot type
+		p <- p + 
+			switch(input$plot_type,
+						 "bar" = geom_col(),                    # Bar chart
+						 "scatter" = geom_point(),              # Scatterplot
+						 "violin" = geom_violin()               # Violin plot
+			) +
+			labs(
+				x = input$x_axis,
+				y = input$y_axis,
+				title = paste("Plot of", input$y_axis, "vs", input$x_axis)
+			) +
+			theme_minimal()  # Optional: use any theme you prefer
+		
+		# Render the plot
+		p
 	})
 	
-	# Download processed data
+	
+	
 	output$download_data <- downloadHandler(
 		filename = function() {
-			paste("df_res", Sys.Date(), ".xlsx", sep = "")
+			paste(if (input$view_type == "raw") "raw_data_" else "analysis_data_", Sys.Date(), ".xlsx", sep = "")
 		},
 		content = function(file) {
 			wb <- createWorkbook()
-			addWorksheet(wb, "Processed Data")
-			writeData(wb, "Processed Data", df_res)
+			addWorksheet(wb, if (input$view_type == "raw") "Raw Data" else "Analysis Data")
+			
+			if (input$view_type == "raw") {
+				writeData(wb, "Raw Data", data_in())
+			} else {
+				writeData(wb, "Analysis Data", df_res_labelled_reactive())
+			}
+			
 			saveWorkbook(wb, file, overwrite = TRUE)
 		}
 	)
+	
 }
 
 # Run the application 
