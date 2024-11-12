43285bcb0c723f536a5a8fdd5a721d5e41f38d0e
Alvaro Gerns <49208751+agerns@users.noreply.github.com>
2024-11-12 13:00:41 +0100

    ignore dply script

--- deployment.R
+++ deployment.R
@@ -13,7 +13,7 @@ rsconnect::deployApp(account='impact-initiatives',paste0(path), appTitle = "ki_a
 # rsconnect::configureApp(account='impact-initiatives',appName = "ki_analysis_app",size="xxlarge", appDir = paste0(path))
 # rsconnect::configureApp(account='impact-initiatives',appName = "reach-ssd-hsm-dashboard",size="xxlarge", appDir = paste0(path))
 
-# rsconnect::showLogs(paste0(path))
+rsconnect::showLogs(paste0(path))
 
 #remove an app
 # rsconnect::appDelete(account = 'impact-initiatives', appName = "geuki_analysis_app")
