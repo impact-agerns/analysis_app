7447459a84d55995ec5d5985f5598bf0e5d33e73
Alvaro Gerns <49208751+agerns@users.noreply.github.com>
2025-03-12 11:23:57 +0100

    functioning second graph

--- src/utils.R
+++ src/utils.R
@@ -101,66 +101,98 @@ plot.select <- function(df=result_long %>% filter(is.na(disag_var_1), is.na(disa
            # title=str_wrap(paste0(unique(df_filtered$label)), width = 65),
            subtitle=paste0("\n", str_wrap(paste0(unique(df_filtered$label) %>% na.omit), width = 65), "\n\n"),
            caption = paste0(unique(df_filtered$resp), " respondents answered the question.")) +
-      theme_minimal() + coord_flip() + theme(plot.title = element_text(), plot.subtitle = element_text(size=9))
+      theme_minimal() + coord_flip() + theme(plot.title = element_text(), 
+                                             plot.subtitle = element_text(size=9),
+                                             panel.grid = element_blank(),  # Remove grid lines
+                                             axis.text.x = element_blank()) # Remove x-axis text)+
     if (sum(df_filtered$count, na.rm=T)>=n_min) {
       plot <- plot + 
       scale_y_continuous(labels=scales::percent_format(), limits=c(0,1), breaks=NULL) + # if you want to display %
-      labs(y="% of respondents") + geom_text(aes(y=mean+0.05, label=paste0(round(100*mean, 1), "%")), size=2.5)
+      labs(y="% of respondents") + geom_text(aes(y=mean+0.07, label=paste0(round(100*mean, 0), "%")), size=2.5)
       } else {
-        plot <- plot + geom_text(aes(y=mean+0.02, label=paste0("n=", count)), size=3)
-      }
-    # if (perc) {plot <- plot + 
-    #     scale_y_continuous(labels=scales::percent_format(), limits=c(0,1)) + # if you want to display %
-    #     labs(y="% of respondents") + geom_text(aes(y=mean+0.05, label=paste0(round(100*mean, 1), "%")), size=2.5)} else {
-    #       plot <- plot + geom_text(aes(y=mean+0.05, label=paste0("n=", count)), size=3)
-    #     }    
+        plot <- plot + 
+          geom_text(aes(y = mean + 0.05, 
+                        label = ifelse(count > 0, paste0(round(mean * 100, 0), "% (", count, ")"), "")), 
+                    size = 2.5)      
+        }
     return(plot)
   }
 }
 # df <- data
 # var <- selected_question
 # n_min=3
-plot.ind_comparison <- function(df=result_long %>% filter(is.na(disag_var_1), is.na(disag_var_2)), var="safety_concern_increase", n_min=threshold){
-  # df_filtered <- df %>% filter(label==var, !is.na(label.choice))
-  df <- data %>%
-    filter(label == selected_question) %>%
-    group_by(choice) %>%
-    summarise(total_percent = round(sum(count)/sum(resp)*100,1)) %>% 
-    slice_max(total_percent, n = 3) %>%
-    inner_join(data, by = c("choice")) %>% 
-    group_by(col, label, choice) %>%
-    arrange(choice, desc(mean), desc(count)) %>% 
-    slice_max(count, n=10)
-  
-  plots <- NULL
-  
-  for (choosen in unique(df$choice)){
-    # print(choosen)
-    # choosen <- "lack_of_water_storage_containers"
-    df_filtered <- df %>% 
-      filter(choice==choosen) 
-    # note <- df_filtered %>% pull(indicator_note) %>% unique %>% na.omit
-    if (nrow(df_filtered)>0){
-      plots[[choosen]] <- df_filtered %>% ggplot(aes(x=reorder(disag_val_1,mean), y=mean))+
-        geom_bar(position='dodge', stat='identity', fill="#EE5859")+
-        scale_x_discrete(labels=~str_wrap(., width = 60)) +
-        # scale_y_continuous(labels=scales::percent_format(), limits=c(0,1), breaks = NULL)+ # to display count only
-        labs(x="", y="% of responses", 
-             title=str_wrap(paste0(unique(df_filtered$label)), width = 65),
-             subtitle=paste0("\nChoice: ", str_wrap(paste0(unique(df_filtered$label.choice) %>% na.omit), width = 65), "\n"),
-             caption = paste0("A total of ",sum(unique(df_filtered$resp)), " respondents answered the question.")) +
-        theme_minimal() + coord_flip() + theme(plot.title = element_text(), plot.subtitle = element_text(size=9),
-                                               panel.grid = element_blank(),  # Remove grid lines
-                                               axis.text.x = element_blank())+ # Remove x-axis text)+
-        labs(y="% and number of respondents") + geom_text(aes(y=mean+0.02, label=paste0(round(100*mean,0), "%")), size=2.5)+
-        geom_text(aes(y=mean+0.05, label=paste0("(n=", count,")")), size=2.5)
-      
-    }
-    
+# plot.ind_comparison <- function(df=result_long %>% filter(is.na(disag_var_1), is.na(disag_var_2)), var="safety_concern_increase", n_min=threshold){
+#   # df_filtered <- df %>% filter(label==var, !is.na(label.choice))
+#   df <- data %>%
+#     filter(label == selected_question) %>%
+#     group_by(choice) %>%
+#     summarise(total_percent = round(sum(count)/sum(resp)*100,1)) %>% 
+#     slice_max(total_percent, n = 3) %>%
+#     inner_join(data, by = c("choice")) %>% 
+#     group_by(col, label, choice) %>%
+#     arrange(choice, desc(mean), desc(count)) %>% 
+#     slice_max(count, n=10)
+#   
+#   plots <- NULL
+#   
+#   for (choosen in unique(df$choice)){
+#     # print(choosen)
+#     # choosen <- "lack_of_water_storage_containers"
+#     df_filtered <- df %>% 
+#       filter(choice==choosen) 
+#     # note <- df_filtered %>% pull(indicator_note) %>% unique %>% na.omit
+#     if (nrow(df_filtered)>0){
+#       plots[[choosen]] <- df_filtered %>% ggplot(aes(x=reorder(disag_val_1,mean), y=mean))+
+#         geom_bar(position='dodge', stat='identity', fill="#EE5859")+
+#         scale_x_discrete(labels=~str_wrap(., width = 60)) +
+#         # scale_y_continuous(labels=scales::percent_format(), limits=c(0,1), breaks = NULL)+ # to display count only
+#         labs(x="", y="% of responses", 
+#              title=str_wrap(paste0(unique(df_filtered$label)), width = 65),
+#              subtitle=paste0("\nChoice: ", str_wrap(paste0(unique(df_filtered$label.choice) %>% na.omit), width = 65), "\n"),
+#              caption = paste0("A total of ",sum(unique(df_filtered$resp)), " respondents answered the question.")) +
+#         theme_minimal() + coord_flip() + theme(plot.title = element_text(), plot.subtitle = element_text(size=9),
+#                                                panel.grid = element_blank(),  # Remove grid lines
+#                                                axis.text.x = element_blank())+ # Remove x-axis text)+
+#         labs(y="% and number of respondents") +
+#           geom_text(aes(y = mean + 0.04, 
+#                         label = ifelse(count > 0, paste0(round(mean * 100, 0), "% (", count, ")"), "")), 
+#                     size = 2.5)
+#       
+#     }
+#     
+#   }
+#   invisible(plots)
+# # return(plots)
+# }
+
+
+# Function to create plots for a given choice
+plot.ind_comparison <- function(df_filtered, choosen) {
+  if (nrow(df_filtered) > 0) {
+    return(
+      ggplot(df_filtered, aes(x=reorder(disag_val_1, mean), y=mean)) +
+        geom_bar(position='dodge', stat='identity', fill="#EE5859") +
+        scale_x_discrete(labels = ~str_wrap(., width = 60)) +
+        labs(x = "", y = "% of responses", 
+             title = str_wrap(paste0(unique(df_filtered$label)), width = 65),
+             subtitle = paste0("\nChoice: ", 
+                               str_wrap(paste0(unique(df_filtered$label.choice) %>% na.omit), width = 65), "\n"),
+             caption = paste0("A total of ", sum(unique(df_filtered$resp)), " respondents answered the question.")) +
+        theme_minimal() +
+        coord_flip() +
+        theme(plot.title = element_text(),
+              plot.subtitle = element_text(size=9),
+              panel.grid = element_blank(),
+              axis.text.x = element_blank()) +
+        geom_text(aes(y = mean + 0.04, 
+                      label = ifelse(count > 0, paste0(round(mean * 100, 0), "% (", count, ")"), "")), 
+                  size = 2.5)
+    )
+  } else {
+    return(NULL)  # Return NULL if no data, avoiding empty plots
   }
-  invisible(plots)
-# return(plots)
 }
+
 
 
 # produce histogram for numerical variables
